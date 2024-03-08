114c0866d905b65ab86241da1f760de9e42e7776
xiao <2186840198@qq.com>
2024-03-08 20:57:39 +0800

    第一天

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -71,4 +72,14 @@ public class EmployeeController {
         return Result.success();
     }
 
+
+    @PostMapping
+    public Result<String> login(@RequestBody EmployeeDTO employeeDTO){
+
+        employeeService.save(employeeDTO);
+
+
+
+        return Result.success("新增成功");
+    }
 }
